7c8799ef20e4f28c5729ec72e318d96260aa6447
SocialAnxietier2 <namanbhatnagar06@gmail.com>
2025-11-30 20:36:31 +0530

    Assignment 7

---
+++ Assignment 7/A7Q2.cpp
@@ -0,0 +1,87 @@
+#include <iostream>
+using namespace std;
+
+int a[50], n;
+
+void input() {
+    cout << "How many numbers: ";
+    cin >> n;
+    cout << "Enter numbers:\n";
+    for (int i = 0; i < n; i++) cin >> a[i];
+}
+
+void show() {
+    for (int i = 0; i < n; i++) cout << a[i] << " ";
+    cout << "\n";
+}
+
+void selectionSort() {
+    for (int i = 0; i < n - 1; i++) {
+        int p = i;
+        for (int j = i + 1; j < n; j++)
+            if (a[j] < a[p]) p = j;
+        int t = a[i];
+        a[i] = a[p];
+        a[p] = t;
+    }
+}
+
+void insertionSort() {
+    for (int i = 1; i < n; i++) {
+        int k = a[i];
+        int j = i - 1;
+        while (j >= 0 && a[j] > k) {
+            a[j + 1] = a[j];
+            j--;
+        }
+        a[j + 1] = k;
+    }
+}
+
+void bubbleSort() {
+    for (int i = 0; i < n - 1; i++) {
+        for (int j = 0; j < n - i - 1; j++) {
+            if (a[j] > a[j + 1]) {
+                int t = a[j];
+                a[j] = a[j + 1];
+                a[j + 1] = t;
+            }
+        }
+    }
+}
+
+void ImprovedSelectionSort() {
+    int l = 0, r = n - 1;
+    while (l < r) {
+        int minPos = l, maxPos = r;
+        for (int i = l; i <= r; i++) {
+            if (a[i] < a[minPos]) minPos = i;
+            if (a[i] > a[maxPos]) maxPos = i;
+        }
+        int t = a[l];
+        a[l] = a[minPos];
+        a[minPos] = t;
+        if (maxPos == l) maxPos = minPos;
+        t = a[r];
+        a[r] = a[maxPos];
+        a[maxPos] = t;
+        l++;
+        r--;
+    }
+}
+
+int main() {
+    int ch;
+    while (true) {
+        cout << "\n1.Input\n2.Show\n3.Selection Sort\n4.Insertion Sort\n5.Bubble Sort\n6.Improved Selection Sort\n7.Exit\nChoose: ";
+        cin >> ch;
+
+        if (ch == 1) input();
+        else if (ch == 2) show();
+        else if (ch == 3) { selectionSort(); show(); }
+        else if (ch == 4) { insertionSort(); show(); }
+        else if (ch == 5) { bubbleSort(); show(); }
+        else if (ch == 6) { ImprovedSelectionSort(); show(); }
+        else if (ch == 7) break;
+    }
+}
